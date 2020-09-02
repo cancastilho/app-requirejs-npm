75b55f095b99e5a1e046772a47abea15a6f815ef
Carlos Nantes <cancastilho@gmail.com>
2020-09-02 00:44:55 -0300

    Inclui configuração inicial de build. Use npm build.

---
+++ build.js
@@ -0,0 +1,11 @@
+({
+    baseUrl: ".",
+    paths: {
+        'jquery': './node_modules/jquery/dist/jquery.min',
+        "text": "./node_modules/text/text",
+        'app':'js/app',
+        'telas':'./telas'
+    },
+    name: "js/main",
+    out: "built/main-built.js"
+})
